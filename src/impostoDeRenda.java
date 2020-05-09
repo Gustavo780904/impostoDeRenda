3ea38018d03b5ff45e9cc3c406f1f03312bc03ed
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-09 01:09:09 -0300

    Consolidado de renda

--- src/impostoDeRenda.java
+++ src/impostoDeRenda.java
@@ -6,8 +6,11 @@ public class impostoDeRenda {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		double salario, servico, capital, medico, educacional;
+
+		double salario, servico, capital, medico, educacional, impostoSal, impostoServ, impostoGcap, impostoBruto,
+				impostoDevido, deducao;
+
+		// Leitura dos dados
 		System.out.print("Renda anual com salário: ");
 		salario = sc.nextDouble();
 		System.out.print("Renda anual com prestação de serviço: ");
@@ -18,11 +21,28 @@ public class impostoDeRenda {
 		medico = sc.nextDouble();
 		System.out.print("Gastos educacionais: ");
 		educacional = sc.nextDouble();
-		
-		
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
+		System.out.println();
+		System.out.println("CONSOLIDADO DE RENDA:");
+		// Cálculos
+
+		if (salario >= 36000.00 && salario <= 60000.00) {
+			impostoSal = salario * 0.1;
+		} 
+		else if (salario > 60000.00) {
+			impostoSal = salario * 0.2;
+		} 
+		else {
+			impostoSal = 0.0;
+		}
+		System.out.printf("Imposto sobre salário: %.2f%n", impostoSal);
+		impostoServ = servico * 0.15;
+		System.out.printf("Imposto sobre serviços: %.2f%n", impostoServ);
+		impostoGcap = capital * 0.2;
+		System.out.printf("Imposto sobre ganho de capital: %.2f%n", impostoGcap);
+
 		sc.close();
-		
+
 	}
-
 }
-
